bdcc89b570402dd46fa6bd086adc59de5d93cea9
gysagsohn <gysagsohn@hotmail.com>
2024-02-10 11:36:39 +1100

    Create cli controller

--- models/user.py
+++ models/user.py
@@ -1,17 +1,18 @@
-from init import db, ma 
+from init import db, ma
 
 class User(db.Model):
-    __tablename__="users"
+    __tablename__ = "users"
 
-    id = db.Colum(db.Integer, primary_key=True)
-    name = db.Colum(db.String)
-    email = db.Colum(db.String, nullable=False, unique=True)
-    password = db.Colum(db.String, nullable=False)
-    is_admin = db.Colum(db.Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
 
 class UserSchema(ma.Schema):
-        class Meta:
-              fields = ('id', 'name', 'email', 'password', 'is_admin')
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
 
-user_schema = UserSchema(exclude=['password'])
-users_schema = UserSchema(many=True, exclude=['password'])
+user_schema = UserSchema(exclude=['password']) # {}
+users_schema = UserSchema(many=True, exclude=['password']) # [{}, {}, {}]
