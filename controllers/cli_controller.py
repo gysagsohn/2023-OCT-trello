668a2b142e9acd427f04eef8a85d3c7f5872236e
gysagsohn <gysagsohn@hotmail.com>
2024-02-15 20:39:44 +1100

    Add cards data for seeding the cards table

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -1,6 +1,10 @@
+from datetime import date
+
 from flask import Blueprint
+
 from init import db, bcrypt
 from models.user import User
+from models.card import Card
 
 db_commands = Blueprint('db', __name__)
 
@@ -30,6 +34,44 @@ def seed_tables():
     ]
     
     db.session.add_all(users)
+
+    cards = [
+        Card(
+            title="Card 1",
+            description="Card 1 desc",
+            date=date.today(),
+            status="To Do",
+            priority="High",
+            user=users[0]
+        ),
+        Card(
+            title="Card 2",
+            description="Card 2 desc",
+            date=date.today(),
+            status="Ongoing",
+            priority="High",
+            user=users[0]
+        ),
+        Card(
+            title="Card 3",
+            description="Card 3 desc",
+            date=date.today(),
+            status="Ongoing",
+            priority="Medium",
+            user=users[1]
+        ),
+        Card(
+            title="Card 4",
+            description="Card 4 desc",
+            date=date.today(),
+            status="Done",
+            priority="Low",
+            user=users[1]
+        ),
+    ]
+
+    db.session.add_all(cards)
+
     db.session.commit()
 
     print("Tables seeded")
